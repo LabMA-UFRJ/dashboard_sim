36d21e7f0a40aad34b3fd934ae217060186bfdd8
Gabriel Maia <maiasena.gabriel@matematica.ufrj.br>
2026-06-18 13:45:55 -0300

    Create microdados_resumido.sql

---
+++ src/SQL/paridade_datasus/microdados_resumido.sql
@@ -0,0 +1,4 @@
+CREATE TABLE MICRODADOS_RESUMIDO AS
+select data_nascimento, data_obito,sexo, ocupacao, escolaridade_2010, causa_basica,  estado_civil, raca_cor, id_municipio_residencia , status_do_epidem, naturalidade, sifla_uf
+from MICRODADOS;
+
